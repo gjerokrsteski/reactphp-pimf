370d256756a4096b6426201ceced66898a879487
Gjero Krsteski <krsteski@cleverbridge.com>
2016-12-16 14:10:18 +0100

    ignore docker kill signal and check tests success

--- test.sh
+++ test.sh
@@ -3,4 +3,10 @@
 # run tests
 docker exec -ti reactphp-pimf-api curl -LsS http://codeception.com/codecept.phar -o /usr/local/bin/codecept
 docker exec -ti reactphp-pimf-api chmod a+x /usr/local/bin/codecept
-docker exec -ti reactphp-pimf-api codecept run --colors
+test_ok=$(docker exec -ti reactphp-pimf-api codecept run | grep "OK" | wc -l)
+
+if [[ ${test_ok} -ne "1" ]]; then
+    exit 1
+fi
+
+echo 0
