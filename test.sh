088f5200fd36270d17e607b5888a234528bf785d
Gjero Krsteski <krsteski@cleverbridge.com>
2016-12-16 14:13:51 +0100

    view test response to be informative

--- test.sh
+++ test.sh
@@ -3,7 +3,10 @@
 # run tests
 docker exec -ti reactphp-pimf-api curl -LsS http://codeception.com/codecept.phar -o /usr/local/bin/codecept
 docker exec -ti reactphp-pimf-api chmod a+x /usr/local/bin/codecept
-test_ok=$(docker exec -ti reactphp-pimf-api codecept run | grep "OK" | wc -l)
+tests_response=$(docker exec -ti reactphp-pimf-api codecept run)
+test_ok=$(echo "$tests_response" | grep "OK" | wc -l)
+
+echo "$tests_response"
 
 if [[ ${test_ok} -ne "1" ]]; then
     exit 1
