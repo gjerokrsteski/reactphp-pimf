565c17e5c23c26d813e013884e0582204ff98870
Gjero Krsteski <krsteski@cleverbridge.com>
2017-01-04 17:41:09 +0100

    add test for handle the request content type header

--- tests/api/Create/ArticleWithUsupportedMediaTypeCept.php
+++ tests/api/Create/ArticleWithUsupportedMediaTypeCept.php
@@ -1,7 +1,7 @@
 <?php
 $I = new ApiTester($scenario);
 $I->wantTo('create a article via API using unsupported media type');
-$I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
-$I->sendPOST('/articles', ['title' => '', 'content' => ['test'=>'content']]);
-$I->seeResponseCodeIs(400);
+$I->haveHttpHeader('Content-Type', 'application/NOT-JSON');
+$I->sendPOST('/articles', ['title' => 'test', 'content' => ['test'=>'content']]);
+$I->seeResponseCodeIs(415);
 
